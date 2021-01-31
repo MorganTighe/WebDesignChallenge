849486b47b19b1cc4f67cb3fc4f0df679a3c2aff
Morgan Tighe <morgan.tighe@gmail.com>
2021-01-31 15:44:24 -0600

    Trying to move around folders to make page work

--- WebVisualizations/Assets/Assets/take two/app.py
+++ WebVisualizations/Assets/Assets/take two/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # create route that renders index.html template
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("menudraft.html")
 
 @app.route("/data")
 def data():
